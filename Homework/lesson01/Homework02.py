2329bcb524eb239a1fd08ef0636d2fb16d587250
a.shishmarev89@gmail.com <Burda904>
2022-09-08 17:52:27 +0900

    Практическое задание 2 - использовать форматирование строк

---
+++ Homework/lesson01/Homework02.py
@@ -0,0 +1,3 @@
+time = int(input("Введите время в секундах: "))
+print('{}'.format(time / 60))
+print('{}'.format(time / 3600))
--- lesson01/example.py
+++ lesson01/example.py
@@ -1,6 +1,3 @@
-number = int(input('Введите целое число от 0 до 9: '))
-number2 = int(input('Введите целое число: '))
-while number < number2:
-    print(number)
-    number = number + 1
-    print('Программа завершена успешно!')
+my_var=int(input("Введите число: ")) + 5
+print(my_var)
+
