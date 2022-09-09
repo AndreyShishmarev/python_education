8003bbacc422131850095c136861cd49b66f6894
a.shishmarev89@gmail.com <Burda904>
2022-09-09 15:29:07 +0900

    Практическое задание 2 - использовать форматирование строк

--- Homework/lesson01/Homework02.py
+++ Homework/lesson01/Homework02.py
@@ -1,3 +1,4 @@
 time = int(input("Введите время в секундах: "))
-print('{}'.format(time / 60))
-print('{}'.format(time / 3600))
+# print('{}'.format(time / 60))
+# print('{}'.format(time / 3600))
+print(f'{time // 3600}:{time // 60}:{time % 60}')
