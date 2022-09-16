57914459a7eb15827e85c75d67d7538aafcab9f3
a.shishmarev89@gmail.com <Burda904>
2022-09-16 13:25:03 +0900

    2.	Для списка реализовать обмен значений соседних элементов. Значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т. д. При нечётном количестве элементов последний сохранить на своём месте. Для заполнения списка элементов нужно использовать функцию input().

---
+++ Homework/Lesson02/l2hw02.py
@@ -0,0 +1,5 @@
+volume_list = []
+for element in range(int(input('Введите количество элементов списка:'))):
+    print('Элемент списка №', element + 1)
+    volume_list.append(input('Введите значение: '))
+print(volume_list)
