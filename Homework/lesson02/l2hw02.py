30d6a91cbf566f0c5067993e54c8d7ef67ee4ef1
a.shishmarev89@gmail.com <Burda904>
2022-09-19 18:21:19 +0900

    3.	Пользователь вводит месяц в виде целого числа от 1 до 12. Сообщить, к какому времени года относится месяц (зима, весна, лето, осень). Напишите решения через list и dict.

--- Homework/lesson02/l2hw02.py
+++ Homework/lesson02/l2hw02.py
@@ -5,5 +5,5 @@ for element in range(int(input('Введите количество элемен
 print(volume_list)
 
 for el in range(0, len(volume_list)-1, 2):
-     volume_list[el], volume_list[el+1] = volume_list[el+1], volume_list[el]
+    volume_list[el], volume_list[el+1] = volume_list[el+1], volume_list[el]
 print(volume_list)
---
+++ Homework/lesson02/l2hw03.py
@@ -0,0 +1,11 @@
+month_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+month = input('Введите номер месяца:')
+for month in month_list:
+    if month == month_list[1-2 or 12]:
+        print('Зима')
+    elif month == month_list[3 or 4 or 5]:
+        print('Весна')
+    elif month == month_list[5 or 6 or 7]:
+        print('Лето')
+    elif month == month_list[8 or 9 or 10]:
+        print('Осень')
