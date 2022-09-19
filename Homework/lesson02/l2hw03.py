c245d7d01957f316c0d666d8069b687c4529347f
AndreyShishmarev <36426818+AndreyShishmarev@users.noreply.github.com>
2022-09-19 22:41:54 +0900

    Что то получилось!

--- Homework/lesson02/l2hw03.py
+++ Homework/lesson02/l2hw03.py
@@ -1,11 +1,12 @@
 month_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-month = input('Введите номер месяца:')
-for month in month_list:
-    if month == month_list[1-2 or 12]:
-        print('Зима')
-    elif month == month_list[3 or 4 or 5]:
-        print('Весна')
-    elif month == month_list[5 or 6 or 7]:
-        print('Лето')
-    elif month == month_list[8 or 9 or 10]:
-        print('Осень')
+month = int(input('Введите номер месяца:'))
+if month == month_list[0] or month == month_list[1] or month == month_list[11]:
+    print('Зима')
+elif month == month_list[2] or month == month_list[3] or month == month_list[4]:
+    print('Весна')
+elif month == month_list[5] or month == month_list[6] or month == month_list[7]:
+    print('Лето')
+elif month == month_list[8] or month == month_list[9] or month == month_list[10]:
+    print('Осень')
+else:
+    print('Не существует такого месяца')
