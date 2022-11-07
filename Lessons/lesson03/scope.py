f78e01106aa6450c09f13f5e60daa8a710451351
a.shishmarev89@gmail.com <Burda904>
2022-11-07 17:16:49 +0900

    Урок №3

--- Lessons/lesson03/scope.py
+++ Lessons/lesson03/scope.py
@@ -1,12 +1,26 @@
-number = 1
+#Области видимости функций
+# number = 1
+#
+#
+# def increment(a):
+#     global result
+#     result = a + 1
+#     print(result)
+#
+#
+# increment(number)
+#
+# print(number)
+# print(result)
 
 
-def increment(a):
-    print(a)
-    a += 1
-    print(a)
+def top_func():
+    start = 0
 
+    def inner_func():
+        nonlocal start
+        start += 1
+    inner_func()
+    print(start)
 
-increment(number)
-
-print(number)
+top_func()
