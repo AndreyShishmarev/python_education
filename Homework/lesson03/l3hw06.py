6a6ad3de3dbbf1cebcd6a9c2f3f055cc3fc5c009
a.shishmarev89@gmail.com <Burda904>
2022-12-07 17:17:34 +0900

    Урок_№3_ДЗ_№6

---
+++ Homework/lesson03/l3hw06.py
@@ -0,0 +1,9 @@
+# 6.  функцию int_func(), принимающую слова из маленьких латинских букв и возвращающую их же,
+# но с прописной первой буквой. Например, print(int_func(‘text’)) -> Text.
+
+def int_func(string):
+    string.split()
+    return string.capitalize()
+
+
+print(int_func('text'))
