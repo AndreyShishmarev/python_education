1bbb5af5d3cde038f2afdeb22dfacc4ebfab9d58
a.shishmarev89@gmail.com <Burda904>
2022-12-28 15:52:24 +0900

    Урок_№4 (Реализован нормальный расчет ЗП)

--- Homework/lesson04/l4hw01.py
+++ Homework/lesson04/l4hw01.py
@@ -1,12 +1,20 @@
 # 1.	Реализовать скрипт, в котором должна быть предусмотрена функция расчёта заработной платы сотрудника.
 # Используйте в нём формулу: (выработка в часах*ставка в час) + премия.
 # Во время выполнения расчёта для конкретных значений необходимо запускать скрипт с параметрами.
-def calculate_salary(production_in_hours, rate_per_hour, premium):
-    salary = (production_in_hours * rate_per_hour)
-    try:
-        return salary + salary / 100 * premium
-    except TypeError:
-        return
 
+from sys import argv
 
-print(calculate_salary(10, 10, 60))
+file_name, production_in_hours, rate_per_hour, premium = argv
+salary = int(production_in_hours) * int(rate_per_hour)
+calculate_salary = salary + salary / 100 * int(premium)
+print(calculate_salary)
+
+# def calculate_salary(production_in_hours, rate_per_hour, premium):
+#     salary = (production_in_hours * rate_per_hour)
+#     try:
+#         return salary + salary / 100 * premium
+#     except TypeError:
+#         return
+#
+#
+# print(calculate_salary(10, 10, 60))
