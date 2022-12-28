b2daca3760a4fee8bb01cb4139f9aa24f280ba2c
a.shishmarev89@gmail.com <Burda904>
2022-12-28 15:53:38 +0900

    Урок_№4 (Реализован скрипт с параметрами)

--- Homework/lesson04/l4hw01.py
+++ Homework/lesson04/l4hw01.py
@@ -8,13 +8,3 @@ file_name, production_in_hours, rate_per_hour, premium = argv
 salary = int(production_in_hours) * int(rate_per_hour)
 calculate_salary = salary + salary / 100 * int(premium)
 print(calculate_salary)
-
-# def calculate_salary(production_in_hours, rate_per_hour, premium):
-#     salary = (production_in_hours * rate_per_hour)
-#     try:
-#         return salary + salary / 100 * premium
-#     except TypeError:
-#         return
-#
-#
-# print(calculate_salary(10, 10, 60))
