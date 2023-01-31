4dd6c61332efc4c9343a66b544cbc1e85596fb29
a.shishmarev89@gmail.com <Burda904>
2023-01-31 17:42:53 +0900

    Урок 5 ДЗ №3

--- Homework/lesson05/l5hw03.py
+++ Homework/lesson05/l5hw03.py
@@ -5,3 +5,13 @@
 # Пример файла:
 # Иванов 23543.12
 # Петров 13749.32
+my_file = open('l5hw03.txt', 'r')
+
+for data in my_file.readlines():
+    list_values = int(data.split()[1])
+    # average = sum(list_values) / len(data.split()[1])
+    print(list_values)
+    # lastname, salary = data.split()
+    # if int(salary) < 20000:
+    #     print(data)
+    my_file.close()
