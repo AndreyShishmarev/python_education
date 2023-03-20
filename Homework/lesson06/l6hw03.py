70b3b6c6509850b718bdd935f16097a0d4f819b0
a.shishmarev89@gmail.com <Burda904>
2023-03-20 13:17:39 +0900

    Урок 6 ДЗ №3
    Signed-off-by: a.shishmarev89@gmail.com <Burda904>

--- Homework/lesson06/l6hw03.py
+++ Homework/lesson06/l6hw03.py
@@ -9,9 +9,34 @@
 # ●	проверить работу примера на реальных данных: создать экземпляры класса Position,
 # передать данные, проверить значения атрибутов, вызвать методы экземпляров.
 
-
 class Worker:
+    wage = int
+    bonus = int
     name = str
     surname = str
     position = str
-    _income = {"wage": 25300, "bonus": 11000}
+    _income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income["wage"] = wage
+        self._income["bonus"] = bonus
+
+    def get_full_name(self):
+        print(self.name, self.surname, self.position)
+
+    def get_total_income(self):
+        print(self._income["wage"] + self._income["bonus"])
+
+
+p = Position("Jonny", "Walker", "CEO", 23600, 11000)
+p.get_full_name()
+p.get_total_income()
+
+p2 = Position("Danny", "Koopa", "Streamer", 10000000, 1)
+p2.get_full_name()
+p2.get_total_income()
