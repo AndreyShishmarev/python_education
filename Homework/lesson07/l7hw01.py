bd3af8d3a64ad995532e3445a1e2d28a474fd2ee
a.shishmarev89@gmail.com <Burda904>
2023-04-05 17:42:08 +0900

    Урок 7 ДЗ №1 не завершено
    Signed-off-by: a.shishmarev89@gmail.com <Burda904>

---
+++ Homework/lesson07/l7hw01.py
@@ -0,0 +1,18 @@
+class Matrix:
+    # n, m = 3, 2
+    matrix = []
+    # for i in range(n):
+    #     row = []
+    #     for j in range(m):
+    #         row.append(input())
+    #     matrix.append(row)
+    #
+    # for t in range(n):
+    #     for k in range(m):
+    #         print(matrix[t][k], end=' ')
+    #     print()
+
+    # m = Matrix()
+    # print(m.matrix)
+
+    print(int(matrix[31, 37, 51][22, 43, 86]), end=' ')
