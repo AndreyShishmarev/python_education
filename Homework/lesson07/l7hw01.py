163f9e51cd8854b1bcfdea9b5781b33c8e71a778
a.shishmarev89@gmail.com <Burda904>
2023-04-07 15:58:02 +0900

    Урок 7 ДЗ №1 не завершено
    Signed-off-by: a.shishmarev89@gmail.com <Burda904>

--- Homework/lesson07/l7hw01.py
+++ Homework/lesson07/l7hw01.py
@@ -1,18 +1,21 @@
 class Matrix:
-    # n, m = 3, 2
-    matrix = []
-    # for i in range(n):
-    #     row = []
-    #     for j in range(m):
-    #         row.append(input())
-    #     matrix.append(row)
-    #
-    # for t in range(n):
-    #     for k in range(m):
-    #         print(matrix[t][k], end=' ')
-    #     print()
-    #
-    # m = Matrix()
-    # print(m.matrix)
+    n = int
+    m = int
 
-    print(int(matrix[31, 37, 51][22, 43, 86]), end=' ')
+    def __init__(self, n, m):
+        self.n = n
+        self.m = m
+        matrix_list = []
+        for i in range(n):
+            row = []
+            for j in range(m):
+                row.append(input())
+            matrix_list.append(row)
+
+        for t in range(n):
+            for k in range(m):
+                print(matrix_list[t][k], end=' ')
+            print()
+
+
+matrix = Matrix(3, 3)
