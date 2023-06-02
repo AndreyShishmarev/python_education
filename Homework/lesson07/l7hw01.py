f914f33a301d4abf6c65c0b8c05aa37640295b2f
AndreyShishmarev <36426818+AndreyShishmarev@users.noreply.github.com>
2023-06-02 17:38:11 +0900

    Урок_№5_ДЗ_№7

--- Homework/lesson07/l7hw01.py
+++ Homework/lesson07/l7hw01.py
@@ -2,16 +2,20 @@ class Matrix:
 
     def __init__(self, matrix_list):
         self.matrix_list = matrix_list
-
-    def __str__(self):
-
         for i in range(len(self.matrix_list)):
             for j in range(len(self.matrix_list[i])):
                 print(self.matrix_list[i][j], end=' ')
             print()
-        # return self.matrix_list
+    # def __str__(self):
+    #
+    #     return
+
+    # def __add__(self, other):
+    #     return Matrix(self.matrix_list + other.matrix_list)
 
 
-mtx = Matrix([[11, 22], [33, 44], [55, 66]])
+mtx1 = Matrix([[11, 22], [33, 44], [55, 66]])
+mtx2 = Matrix([[12, 23], [34, 45], [56, 67]])
+mtx = mtx1 + mtx2
 # print(mtx.matrix_list)
-print(mtx)
+# print(mtx)
