e4b4d2ee611f3c83161a27e438ca812aafd83582
a.shishmarev89@gmail.com <Burda904>
2023-06-05 18:03:48 +0900

    Урок 7 ДЗ №1 не завершено
    Signed-off-by: a.shishmarev89@gmail.com <Burda904>

--- Homework/lesson07/l7hw01.py
+++ Homework/lesson07/l7hw01.py
@@ -2,20 +2,21 @@ class Matrix:
 
     def __init__(self, matrix_list):
         self.matrix_list = matrix_list
+
+    def __add__(self, other):
+        return Matrix(self.matrix_list + other.matrix_list)
+
+    def __str__(self):
+        result = ''
         for i in range(len(self.matrix_list)):
             for j in range(len(self.matrix_list[i])):
                 print(self.matrix_list[i][j], end=' ')
             print()
-    # def __str__(self):
-    #
-    #     return
-
-    # def __add__(self, other):
-    #     return Matrix(self.matrix_list + other.matrix_list)
+        return result
 
 
 mtx1 = Matrix([[11, 22], [33, 44], [55, 66]])
-mtx2 = Matrix([[12, 23], [34, 45], [56, 67]])
-# mtx = mtx1 + mtx2
-# print(mtx.matrix_list)
-# print(mtx)
+mtx2 = Matrix([[20, 30], [40, 50], [60, 70]])
+mtx_sum = mtx1 + mtx2
+print(mtx_sum)
+# result = map(sum, zip(mtx1,mtx2))
