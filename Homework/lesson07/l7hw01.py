d615f5f46cec2fa78e03f6ec3346452993523c8f
AndreyShishmarev <36426818+AndreyShishmarev@users.noreply.github.com>
2023-06-09 15:54:08 +0900

    Урок_№7_ДЗ_№1 не решено

--- Homework/lesson07/l7hw01.py
+++ Homework/lesson07/l7hw01.py
@@ -4,15 +4,18 @@ class Matrix:
         self.matrix_list = matrix_list
 
     def __add__(self, other):
-        return Matrix(self.matrix_list + other.matrix_list)
-
-    def __str__(self):
-        result = ''
+        result = []
         for i in range(len(self.matrix_list)):
             for j in range(len(self.matrix_list[i])):
-                print(self.matrix_list[i][j], end=' ')
-            print()
-        return result
+                matrix_sum = self.matrix_list + other.matrix_list
+                result.append(matrix_sum)
+                # print(self.matrix_list[i][j], end=' ')
+            print(result)
+        return Matrix(result)
+
+    def __str__(self):
+        return f"Матрица:\n{' '.join(map(str, self.matrix_list))} \n{' '.join(map(str, self.matrix_list[1]))} \n" \
+               f"{' '.join(map(str, self.matrix_list[2]))} \n "
 
 
 mtx1 = Matrix([[11, 22], [33, 44], [55, 66]])
